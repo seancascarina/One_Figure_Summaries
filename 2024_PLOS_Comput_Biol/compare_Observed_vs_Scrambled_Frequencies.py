a519c7efd4e7c203055a5b5ffc98f700a2909f5e
Sean Cascarina <31107448+seancascarina@users.noreply.github.com>
2025-02-23 10:30:02 -0700

    Add comments and docstrings

--- 2024_PLOS_Comput_Biol/compare_Observed_vs_Scrambled_Frequencies.py
+++ 2024_PLOS_Comput_Biol/compare_Observed_vs_Scrambled_Frequencies.py
@@ -2,6 +2,8 @@
 from scipy.stats import fisher_exact
 import math
 import mpmath
+
+# CREATE LIST OF STRINGS FOR ALL 400 LCD CLASSES
 amino_acids = 'ACDEFGHIKLMNPQRSTVWY'
 aa_strings = [aa for aa in amino_acids]
 for res1 in amino_acids:
@@ -12,12 +14,15 @@ for res1 in amino_acids:
         
 def main():
 
+    # GET LCD FREQUENCY DATA
     obs_df, org_to_domain = get_frequencies('TableS1_LCDfrequency_NumberOfProtsWithLCDs_Humans_Malaria_Only.tsv')
     scr_df, org_to_domain_scr = get_frequencies('TableS1_LCDfrequency_NumberOfProtsWithLCDs_SCRAMBLED_Humans_Malaria_Only.tsv')
 
+    # PREP OUTPUT FILE
     output = open('Observed_vs_Scrambled_FisherExact_Results.tsv', 'w')
     output.write('\t'.join(['Proteome', 'Domain of Life', 'LCD Class', '# of Proteins with LCDs, Actual Proteome ("Observed")', '# of Proteins with LCDs, Scrambled Proteome', 'Total Proteins in Proteome', 'OddsRatio', 'lnOR', 'Fold Change (# in actual proteome / # in scrambled proteome)', '95% Confidence Interval (lower bound, upper bound)', '95% Confidence Interval for Odds Ratio Excludes 1?', 'p-value', 'Sidak-Holm Corrected p-value', 'Biased OddsRatio', 'Biased lnOR (when necessary)', 'Biased Fold Change [(# in actual proteome + 1) / (# in scrambled proteome + 1)]', 'Biased 95% Confidence Interval (lower bound, upper bound)', 'Biased Raw p-value (when necessary)']) + '\n')
     
+    # LOOP OVER PROTEOMES TO MAKE PLOTS FOR
     for proteome in obs_df:
         scr_label = proteome + '_SCRAMBLED'
         if scr_label in scr_df:
@@ -27,14 +32,18 @@ def main():
             data_df = {}
             biased_df = {}
             domain = org_to_domain[proteome]
+            
+            # LOOP OVER 400 LCD CLASSES
             for lcd_class in aa_strings:
                 obs = obs_df[proteome][lcd_class]
                 scr = scr_df[scr_label][lcd_class]
                 total_prots_obs = obs_df[proteome]['Total Proteins']
                 total_prots_scr = scr_df[scr_label]['Total Proteins']
+                
+                # INITIALIZE BIAS VARIABLES AS N/A (CASES IN WHICH BIASED ESTIMATES ARE NOT NECESSARY)
                 biased_oddsratio, biased_lnOR, biased_upper_CI, biased_lower_CI, biased_pval = ['N/A']*5
                 
-                if obs == 0 and scr == 0:
+                if obs == 0 and scr == 0:   # NEED BIASED ESTIMATES DUE TO ZEROS
                     data_line = [str(x) for x in (proteome, domain, lcd_class, obs, scr, total_prots_obs)] + ['N/A']*6
                     data_df[lcd_class] = data_line
                     biased_oddsratio, biased_lnOR, biased_upper_CI, biased_lower_CI, biased_pval = calc_lnOR(obs+1, total_prots_obs+1, scr+1, total_prots_scr+1, proteome)
@@ -48,6 +57,7 @@ def main():
 
                     continue
 
+                # CALCULATE ODDS RATIO AND CONFIDENCE INTERVALS
                 oddsratio, lnOR, upper_CI, lower_CI, pval = calc_lnOR(obs, total_prots_obs, scr, total_prots_scr, proteome)
 
                 if upper_CI != 'N/A' and lower_CI != 'N/A':
@@ -57,14 +67,14 @@ def main():
                 else:
                     is_in_CI = 'N/A'
 
-                if obs == 0 or scr == 0:
+                if obs == 0 or scr == 0:    # FOR BIASED ESTIMATES
                     fold_change = 'N/A'
                     biased_oddsratio, biased_lnOR, biased_upper_CI, biased_lower_CI, biased_pval = calc_lnOR(obs+1, total_prots_obs+1, scr+1, total_prots_scr+1, proteome)
                     biased_fold_change = (obs+1) / (scr+1)
-                else:
+                else:   # FOR NON-BIASED ESTIMATES
                     fold_change = obs / scr
                     biased_fold_change = 'N/A'
-                    
+
                 if oddsratio == 'N/A':
                     ci = 'N/A'
                 else:
@@ -73,6 +83,8 @@ def main():
                     biased_ci = 'N/A'
                 else:
                     biased_ci = (biased_lower_CI, biased_upper_CI)
+                    
+                # CREATE DATA STRING FOR OUTPUT
                 data_line = [str(x) for x in (proteome, domain, lcd_class, obs, scr, total_prots_obs, oddsratio, lnOR, fold_change, ci, is_in_CI, pval)]
 
                 data_df[lcd_class] = data_line
@@ -82,11 +94,13 @@ def main():
                     pvals.append(pval)
                     lcd_classes.append(lcd_class)
 
+            # CORRECT P-VALUES FOR MULTIPLE HYPOTHESIS TESTS
             if len(pvals) == 0:
                 corrected_pvals = []
             else:
                 corrected_pvals = sidak_correction(pvals)
             
+            # PREPARE DATA FOR OUTPUT
             for lcd_class in aa_strings:
                 if lcd_class in lcd_classes:
                     index = lcd_classes.index(lcd_class)
@@ -102,7 +116,11 @@ def main():
 
 
 def sidak_correction(pvals):
-
+    """Performs multiple test correction using Sidak-Holm method.
+    Returns:
+        final_pvals = list of corrected p-values, sorted in ascending order.
+    """
+    
     positions = [i for i in range(len(pvals))]
     sorted_pvals, pval_positions = zip(*sorted(zip(pvals, positions)))
     m = len(pvals)
@@ -126,17 +144,29 @@ def sidak_correction(pvals):
     
          
 def calc_lnOR(obs, total_prots_obs, scr, total_prots_scr, proteome):
-
+    """Calculate the natural log of the odds ratio for the LCD frequency for a given
+    LCD class in the original proteome versus scrambled proteome.
+    Returns:
+        oddsratio = float
+        lnOR = natural log of the odds ratio, float
+        upper_CI = upper bound on the 95% confidence interval, float
+        lower_CI = lower bound on the 95% confidence interval, float
+        pval = p-value from Fisher's exact test, float
+    """
     contingency = [ [obs, (total_prots_obs-obs)], [scr, (total_prots_scr-scr)] ]
 
     oddsratio, pval = fisher_exact(contingency, alternative='two-sided')
 
+    # lnOR, oddsratio, AND CONFIDENCE INTERVALS CAN'T BE CALCULATED IF 
+    # OBSERVED OR SCRAMBLED FREQUENCIES ARE ZERO. REQUIRES BIASED ESTIMATION, 
+    # WHICH IS PERFORMED LATER.
     if obs == 0 or scr == 0 or obs == total_prots_obs or scr == total_prots_scr:
         lnOR = 'N/A'
         upper_CI = 'N/A'
         lower_CI = 'N/A'
         oddsratio = 'N/A'
 
+    # CASES WHERE BIASED ESTIMATION IS NOT NEEDED
     else:
         lnOR = math.log(oddsratio)
         
@@ -147,7 +177,15 @@ def calc_lnOR(obs, total_prots_obs, scr, total_prots_scr, proteome):
     
 
 def get_frequencies(file):
-
+    """Get LCD frequencies for each LCD class and organism.
+    Returns:
+        df = multi-dimensional dictionary
+                1st dimension ---> proteome as keys, LCD classes as values
+                2nd dimension ---> LCD classes as keys, # of proteins with that type of LCD as values
+                    This dimension also contains "Total Proteins" key and corresponding # of proteins in the proteome as the value.
+        org_to_domain = mapping dictionary with organism ID as keys and corresponding domain of life as values
+    """
+    
     h = open(file)
     header = h.readline()
     df = {}
--- 2024_PLOS_Comput_Biol/plot_IndividualOrganism_lnORs_and_Pvals.py
+++ 2024_PLOS_Comput_Biol/plot_IndividualOrganism_lnORs_and_Pvals.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import pandas as pd
 import math
 
+# CREATE LIST OF STRINGS FOR ALL 400 LCD CLASSES
 amino_acids = 'ACDEFGHIKLMNPQRSTVWY'
 aa_strings = [aa for aa in amino_acids]
 for res1 in amino_acids:
@@ -12,6 +13,7 @@ for res1 in amino_acids:
             continue
         aa_strings.append(res1+res2)
 
+# PREP GLOBAL VARIABLES AND FILE NAMES
 domains = ['Archaea','Bacteria','Eukaryota','Viruses']
 proteomes_of_interest = ['UP000005640_9606', 'UP000001450_36329']
 fig_labels = {('UP000001450_36329', 'lnOR'):'Fig5A',
@@ -23,18 +25,25 @@ fig_labels = {('UP000001450_36329', 'lnOR'):'Fig5A',
 def main():
 
     domain = 'Eukaryota'
+    
+    # SORTED AMINO ACID ORDERS FOR PLOTTING FROM HIGHEST WHOLE-PROTEOME FREQUENCY TO LOWEST WHOLE-PROTEOME FREQUENCY
     aa_freqs_df, ordered_aas_df = determine_aa_order()
 
+    # LOOP OVER PROTEOMES OF INTEREST TO CREATE SEPARATE PLOTS
     for proteome_of_interest in proteomes_of_interest:
-        pvals_df, lnOR_df, proteome_pvals_df, datalines_df, header_line, pvals_list, lcd_class_list = get_data(proteome_of_interest)
+        
+        # GATHER FISHER'S EXACT TEST RESULTS FOR LCD CLASSES
+        pvals_df, lnOR_df, pvals_list, lcd_class_list = get_data(proteome_of_interest)
 
-        # THIS IS USING THE PRE-CALCULATED HOLM-SIDAK CORRECTED P-VALUES
+        # USE THE PRE-CALCULATED HOLM-SIDAK CORRECTED P-VALUES
         corrected_pvals_df = {lcd_class:pvals_list[i] for i, lcd_class in enumerate(lcd_class_list)}
 
         matrix = []
         ordered_aas = ordered_aas_df[proteome_of_interest]
         df = {aa:[] for aa in ordered_aas}
         lnORs_master = {aa:[] for aa in ordered_aas}
+        
+        # NESTED LOOPS COVER ALL 400 LCD CLASSES
         for res1 in ordered_aas:
             row = []
             for res2 in ordered_aas:
@@ -47,64 +56,90 @@ def main():
 
                 pvals = [float(x) for x in pvals if x !='N/A']
 
-                # USE THIS WHEN USING THE lnOR FOR THE HEATMAP============
+                # STORE THE LOG ODDSRATIOS FOR HEATMAPS
                 if res1 == res2:
                     lnOR = lnOR_df[proteome_of_interest][res1]
                 else:
                     lnOR = lnOR_df[proteome_of_interest][res1+res2]
 
-                # USE FOR CORRECTED LOG PVALS
+                # STORE CORRECTED LOG PVALS FOR HEATMAPS
                 row.append(corrected_pval)
                 df[res1].append(corrected_pval)
                 
                 lnORs_master[res1].append(lnOR)
 
             matrix.append(row)
-            
-        binary_df = {}
-        for i, res in enumerate(list(df.keys())):
-            binary_df[res] = []
-            for j in range(len(df[res])):
-                val = df[res][j]
 
-                if val == 'N/A':
-                    binary_df[res].append(1)    # MASKED VALUE: NO LCDs
-                elif val < 0.05:
-                    binary_df[res].append(3)
-                else:
-                    binary_df[res].append(2)
+        binary_df = get_binary_statistical_significance(df)
                     
         df = {lcd_class:[-math.log10(df[lcd_class][i]) if df[lcd_class][i] != 'N/A' else 0.0 for i in range(len(df[lcd_class]))] for lcd_class in df}
         
+        # PREP DATAFRAMES FOR OUTPUT TO FILE=========================================================#
         df = pd.DataFrame.from_dict(df)
         df['Secondary Amino Acid'] = list(ordered_aas)
         df.set_index('Secondary Amino Acid', inplace=True)
         df.to_csv(proteome_of_interest + '_SecondaryLCDs_Matrix_Log10correctedPvals.tsv', sep='\t')
+        #============================================================================================#
 
+        # MAKE BINARY STATISTICAL SIGNIFICANCE HEATMAP AND OUTPUT DATA TO FILE=======================#
         binary_df = pd.DataFrame.from_dict(binary_df)
         binary_df['Secondary Amino Acid'] = list(ordered_aas)
         binary_df.set_index('Secondary Amino Acid', inplace=True)
         fig_label = fig_labels[ (proteome_of_interest, 'Statistical Significance') ]
         plot_binary_heatmap(binary_df, proteome_of_interest, fig_label)
         binary_df.to_csv(proteome_of_interest + '_SecondaryLCDs_HeatmapMatrix_Log10correctedPvals_BINARY_TABLE.tsv', sep='\t')
-
+        #============================================================================================#
+        
+        # MAKE LOG ODDSRATIO HEATMAP AND OUTPUT DATA TO FILE=========================================#
         lnORs_master = pd.DataFrame.from_dict(lnORs_master)
         lnORs_master['Secondary Amino Acid'] = list(ordered_aas)
         lnORs_master.set_index('Secondary Amino Acid', inplace=True)
         fig_label = fig_labels[ (proteome_of_interest, 'lnOR') ]
         plot_heatmap(lnORs_master, proteome_of_interest, fig_label)
         lnORs_master.to_csv(proteome_of_interest + '_SecondaryLCDs_HeatmapMatrix_lnORs.tsv', sep='\t')
+        #============================================================================================#
         
+        
+def get_binary_statistical_significance(df):
+    """Convert corrected p-values to a binary representation of statistical significance.
+    Also includes a third category for LCD classes for which the organism had zero
+    proteins with that type of LCD.
+    Returns:
+        binary_df = dictionary with LCD classes as keys and label-encoded statistical significance as values
+    """
+    binary_df = {}
+    for i, res in enumerate(list(df.keys())):
+        binary_df[res] = []
+        for j in range(len(df[res])):
+            val = df[res][j]
+
+            if val == 'N/A':
+                binary_df[res].append(1)    # MASKED VALUE: NO LCDs
+            elif val < 0.05:
+                binary_df[res].append(3)
+            else:
+                binary_df[res].append(2)
+                    
+    return binary_df
+    
 
 def get_data(proteome_of_interest):
-
+    """Gather data from Fisher's exact test results file.
+    Returns:
+        df = multi-dimensional dictionary
+                1st dimension ---> domain of life as keys, LCD classes as values
+                2nd dimension ---> LCD classes as keys, corrected p-value as values
+        lnOR_df = multi-dimensional dictionary
+                1st dimension ---> proteome IDs as keys, LCD classes as values
+                2nd dimension ---> LCD classes as keys, log oddsratios as values
+        pvals_list = list of corrected p-values
+        lcd_class_list = list of LCD classes in the same order as pvals_list
+    """
+    
     h = open('Observed_vs_Scrambled_FisherExact_Results.tsv')
     header = h.readline()
     df = {domain:{lcd_class:[] for lcd_class in aa_strings} for domain in domains}
     lnOR_df = {}
-    proteome_pvals_df = {}
-    lcd_freqs_df = {}
-    datalines_df = {}
     pvals_list = []
     lcd_class_list = []
     
@@ -120,8 +155,6 @@ def get_data(proteome_of_interest):
         obs_num = int(items[3])
         scr_num = int(items[4])
         total_prots = int(items[5])
-        
-        datalines_df[lcd_class] = line
 
         lnOR = items[7]
         if lnOR == 'N/A':
@@ -144,11 +177,15 @@ def get_data(proteome_of_interest):
         
     h.close()
     
-    return df, lnOR_df, proteome_pvals_df, datalines_df, header, pvals_list, lcd_class_list
+    return df, lnOR_df, pvals_list, lcd_class_list
     
 
 def plot_heatmap(df, proteome_of_interest, fig_label):
-
+    """Plot heatmap of lnOR values for each LCD class.
+    Returns:
+        None
+    """
+    
     pal = sns.color_palette('gist_heat', as_cmap=True)
     
     cg = sns.heatmap(df, cmap=pal)
@@ -169,7 +206,11 @@ def plot_heatmap(df, proteome_of_interest, fig_label):
     
     
 def plot_binary_heatmap(df, proteome_of_interest, fig_label):
-
+    """Plot heatmap of label-encoded statistical significance indicators.
+    Returns:
+        None
+    """
+    
     pal = ['grey', 'black', 'red']
 
     cg = sns.heatmap(df, cmap=pal)
@@ -191,7 +232,13 @@ def plot_binary_heatmap(df, proteome_of_interest, fig_label):
     
     
 def determine_aa_order():
-
+    """Determines amino acid order for each organism to use in the heatmap axis ticks.
+    This sorts amino acids from most frequent in the proteome to least frequent in the proteome.
+    Returns:
+        df = dictionary with proteome IDs as keys, list of whole-proteome amino acid frequencies as values (alphabetical order by single-letter abbreviation)
+        aa_orders_df = dictionary with proteome IDs as keys, list of amino acids sorted from most frequent to least frequent
+    """
+    
     h = open('Background_AAfrequencies_AllProteomes.tsv')
     header = h.readline()
     df = {}
